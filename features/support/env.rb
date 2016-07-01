0aa2542b2593e1214e078fb4356d420ba64bc689
ryanwyu <ryan.w.yu@gmail.com>
2016-07-01 14:19:08 +0800

    updated to the new framework

--- features/support/env.rb
+++ features/support/env.rb
@@ -14,22 +14,16 @@ require 'selenium-webdriver'
 MAX_SLEEP_SECS = 30
 MAXWAITSECONDS = 30
 ORACLE_ADDRESS = '//your.oracle.db.domain.name.com:1521/your-db-name'
-
-######################################################################
-#### DOKER PARAMETERS                                              ####
-######################################################################
-##  For the $server_url parameter, if the running machine always 
-##  changes, you can just specify the url of the appium server and 
-##  then specify the host-ip mapping by docker run parameter:
-##  --add-host value
-##  where value can be like host:IP
-#---------------------------------------------------------------------
+# $server_url = "http://10.0.0.4:4723/wd/hub"
+## yuwei's mac book address 
+#$server_url = "http://10.97.180.97:4723/wd/hub"
+## shared imac testing machine 
+#$server_url = "http://10.96.211.10:4723/wd/hub"
+## local url value
+#$server_url = "http://127.0.0.1:4723/wd/hub"
 $server_url = "http://appium.server.host:4723/wd/hub"
-## or just specify the ip in this file like below
-$server_url = "http://10.0.0.4:4723/wd/hub"
-######################################################################
 
-$app = "/home/workspace/OneCucumber/zcjb-yy.app"
+$app = "/users/ryanwyu/Downloads/作家助手.app"
 
 # Create a custom World class so we don't pollute `Object` with Appium methods
 class AppiumWorld
@@ -37,18 +31,22 @@ end
 
 # Load the desired configuration from appium.txt, create a driver then
 # Add the methods to the world
+$dev = ENV['IDEVICENAME']
 $caps = Appium.load_appium_txt file: File.expand_path("./../"+ENV['IDEVICENAME']+"/appium.txt", __FILE__), verbose: true
-$caps[:caps][:app] = $app
 $caps[:appium_lib] = {}
 $caps[:appium_lib][:server_url] = $server_url
 
+if $dev != "dummy"
+  $caps[:caps][:app] = $app
+end
 # Cucumber method World()
 World do
   AppiumWorld.new
 end
 
-Before { 
-  if $caps[:caps][:platformName] == "ios"
+Before {
+
+  if $caps[:caps][:platformName] == "ios" or $caps[:caps][:platformName] == "iOS"
     Appium::Driver.new($caps)
     Appium.promote_appium_methods IosSimulator
     $executor = AppExecutor.new(IosSimulator.new)
@@ -57,9 +55,20 @@ Before {
     Appium.promote_appium_methods AndroidSimulator
     $executor = AppExecutor.new(AndroidSimulator.new)
   end
-  $driver.start_driver 
-  $driver.set_wait 60
+
+  if $dev == "dummy"
+    puts "Set in dummy mode, the appium will not be connected!"
+  else
+    $driver.start_driver
+    $driver.set_wait 60
+  end
+
 }
-After { 
-  $driver.driver_quit 
+
+After {
+  if $dev == "demmy"
+  else
+    $driver.driver_quit
+  end
+
 }
